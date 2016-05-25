702a4bd638421545baf9141ebbf29810f02089fd
xapachidi <apachidi.xeniya@gmail.com>
2016-05-25 22:03:32 +0600

    ДЗ 1_2

--- sandbox/src/main/java/kz/stqa/pft/sandbox/HomeWork1_2.java
+++ sandbox/src/main/java/kz/stqa/pft/sandbox/HomeWork1_2.java
@@ -11,15 +11,10 @@ public class HomeWork1_2 {
         Point p1 = new Point(5,2);
         Point p2 = new Point(4,3);
 
-        //p1.x = 5;
-        //p1.y = 2;
-
-        //p2.x = 4;
-        //p2.y = 3;
 
         System.out.println("Координаты точки p1 = " + p1.x + " " + p1.y);
         System.out.println("Координаты точки p2 = " + p2.x + " " + p2.y);
-        System.out.println("Расстояние медлу точками p1 и p2 = " + p1.distance(p2));
+        System.out.println("Расстояние между точками p1 и p2 = " + p1.distance(p2));
     }
 
 
--- sandbox/src/main/java/kz/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/kz/stqa/pft/sandbox/Point.java
@@ -13,7 +13,7 @@ public class Point {
 
     public  double distance(Point p2){
 
-        double dis = Math.sqrt(Math.pow(this.x - this.y, 2) + Math.pow(p2.x - p2.y, 2));
-        return dis;
+        double dist = Math.sqrt(Math.pow(this.x - this.y, 2) + Math.pow(p2.x - p2.y, 2));
+        return dist;
     }
 }
