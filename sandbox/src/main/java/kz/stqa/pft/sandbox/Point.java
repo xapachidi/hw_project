e9332cc439fe7c85e6dd392ecaa2bd24a72a7ebd
xapachidi <apachidi.xeniya@gmail.com>
2016-05-29 13:29:52 +0600

    добавление библиотек

--- sandbox/src/main/java/kz/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/kz/stqa/pft/sandbox/Point.java
@@ -13,7 +13,7 @@ public class Point {
 
     public  double distance(Point p2){
 
-        double dist = Math.sqrt(Math.pow(this.x - this.y, 2) + Math.pow(p2.x - p2.y, 2));
+        double dist = Math.sqrt(Math.pow(this.x - p2.x, 2) + Math.pow(this.y - p2.y, 2));
         return dist;
     }
 }
