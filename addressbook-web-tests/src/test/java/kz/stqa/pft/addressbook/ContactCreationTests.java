20918fb92b18f208e145a0f4a33b5b233e5b6ce2
xapachidi <apachidi.xeniya@gmail.com>
2016-05-31 17:02:00 +0600

    добавление контакта

---
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/ContactCreationTests.java
@@ -0,0 +1,80 @@
+package kz.stqa.pft.addressbook;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.*;
+
+public class ContactCreationTests {
+    FirefoxDriver wd;
+    
+    @BeforeMethod
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/addressbook/index.php");
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).click();
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+    }
+    
+    @Test
+    public void testContactCreation() {
+        initContactCreation();
+        fillContactForm(new ContactData("Петр", "Петрович", "Кузьмин", "ложки.ком", "+89654123654"));
+        returnMainPage();
+    }
+
+    private void returnMainPage() {
+        wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contactData.getFirst_name());
+        wd.findElement(By.name("middlename")).click();
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(contactData.getSecond_name());
+        wd.findElement(By.name("lastname")).click();
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contactData.getLast_name());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(contactData.getCompany());
+        wd.findElement(By.name("mobile")).click();
+        wd.findElement(By.name("mobile")).clear();
+        wd.findElement(By.name("mobile")).sendKeys(contactData.getPhone());
+    }
+
+    private void initContactCreation() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        wd.quit();
+    }
+    
+    public static boolean isAlertPresent(FirefoxDriver wd) {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+}
---
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,37 @@
+package kz.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String first_name;
+    private final String second_name;
+    private final String last_name;
+    private final String company;
+    private final String phone;
+
+    public ContactData(String first_name, String second_name, String last_name, String company, String phone) {
+        this.first_name = first_name;
+        this.second_name = second_name;
+        this.last_name = last_name;
+        this.company = company;
+        this.phone = phone;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getSecond_name() {
+        return second_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+}
