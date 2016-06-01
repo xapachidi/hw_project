4c08ebde1093801eea75a00e202d4aed251fdb76
xapachidi <apachidi.xeniya@gmail.com>
2016-06-01 15:38:59 +0600

    Добавлены хелперы

---
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -0,0 +1,51 @@
+package kz.stqa.pft.addressbook.appmanager;
+
+import kz.stqa.pft.addressbook.model.GroupData;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by xeniya on 6/1/16.
+ */
+public class GroupHelper {
+
+    private FirefoxDriver wd;
+
+    public GroupHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void returnToGroupPage() {
+        wd.findElement(By.cssSelector("div.msgbox")).click();
+        wd.findElement(By.linkText("group page")).click();
+    }
+
+    public void submitGroupCreation() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    public void fillGroupForm(GroupData groupData) {
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).click();
+        wd.findElement(By.name("group_name")).clear();
+        wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+        wd.findElement(By.name("group_header")).click();
+        wd.findElement(By.name("group_header")).clear();
+        wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+        wd.findElement(By.name("group_footer")).click();
+        wd.findElement(By.name("group_footer")).clear();
+        wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    public void initGroupCreation() {
+        wd.findElement(By.name("new")).click();
+    }
+
+    public void deleteSelectedGroups() {
+        wd.findElement(By.name("delete")).click();
+    }
+
+    public void selectGroup() {
+        wd.findElement(By.name("selected[]")).click();
+    }
+}
---
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,20 @@
+package kz.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by xeniya on 6/1/16.
+ */
+public class NavigationHelper {
+
+    private FirefoxDriver wd;
+
+    public NavigationHelper(FirefoxDriver wd) {
+        this.wd = wd;
+    }
+
+    public void gotoGroupPage() {
+        wd.findElement(By.linkText("groups")).click();
+    }
+}
