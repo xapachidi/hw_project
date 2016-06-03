d690d2927408bf6f379c39c8fabd7e7a0fd7c074
xapachidi <apachidi.xeniya@gmail.com>
2016-06-03 17:23:40 +0600

    добавление equals

--- addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/TestBase.java
@@ -1,8 +1,6 @@
 package kz.stqa.pft.addressbook.tests;
 
 import kz.stqa.pft.addressbook.appmanager.ApplicationManager;
-import kz.stqa.pft.addressbook.model.ContactData;
-import org.openqa.selenium.By;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
