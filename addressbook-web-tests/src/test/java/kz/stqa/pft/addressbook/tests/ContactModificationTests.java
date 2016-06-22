365d599ee5739f68d873317887d7d8d4d6540d29
xapachidi <apachidi.xeniya@gmail.com>
2016-06-22 14:08:15 +0600

    исправила модификацию контакта

--- addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -40,11 +40,12 @@ public class ContactModificationTests extends TestBase {
         ContactData modifiedContact = before.iterator().next();
         //app.contact().selectContact(before.size() -1);
         //app.contact().initContactModification(before.size()-1);
-        ContactData contact = new ContactData().withId(modifiedContact.getId()).withFirst_name("Антон").withSecond_name("Петрович").withLast_name("Кузьмин").withCompany("ложки.ком").withPhone("+89654123654").withGroup("test1");
+        ContactData contact = new ContactData().withId(modifiedContact.getId()).withFirst_name("Петр").withSecond_name("Петрович").withLast_name("Кузьмин").withCompany("ложки.ком").withPhone("+89654123654").withGroup("test1");
         app.contact().modifyContact(contact);
         Contacts after = app.contact().all();
+
         assertEquals(after.size(), before.size());
 
-        assertThat(after, equalTo(before.withAdded(contact)));
+        assertThat(after, equalTo(before.without(modifiedContact).withAdded(contact)));
     }
 }
