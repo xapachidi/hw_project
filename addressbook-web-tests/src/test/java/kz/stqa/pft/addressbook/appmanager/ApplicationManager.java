5254b16b34c6de817aefecf00c8e0653d16dc708
xapachidi <apachidi.xeniya@gmail.com>
2016-07-04 21:57:18 +0600

    настройка конфигурационного файла

--- addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -7,7 +7,12 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.NoSuchElementException;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,6 +20,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class ApplicationManager {
 
+    private final Properties properties;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -23,12 +29,16 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser){
         this.browser = browser;
+        properties = new Properties();
+
     }
 
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("srs/test/resources/$s.properties", target))));
 
         if (Objects.equals(browser, BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
@@ -39,12 +49,12 @@ public class ApplicationManager {
         }
 
         wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        wd.get("http://localhost/addressbook/index.php");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         contactHelper = new ContactHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
 
--- addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/TestBase.java
@@ -13,7 +13,8 @@ import org.testng.annotations.BeforeSuite;
  */
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {
