f2613c9c10486e177d0e2fec0384daeaafba105f
xapachidi <apachidi.xeniya@gmail.com>
2016-07-04 21:33:15 +0600

    создание тестового стенда

--- addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/TestBase.java
@@ -13,7 +13,7 @@ import org.testng.annotations.BeforeSuite;
  */
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
+    protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @BeforeSuite
     public void setUp() throws Exception {
