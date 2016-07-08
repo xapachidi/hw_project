9dd2c14c9ee8ccaa2ab7e1fb229e4b25d36e8241
xapachidi <apachidi.xeniya@gmail.com>
2016-07-08 17:47:18 +0600

    orm с контактами

--- addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/model/ContactData.java
@@ -1,30 +1,51 @@
 package kz.stqa.pft.addressbook.model;
 
 import com.google.gson.annotations.Expose;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 
+@Entity
+@Table(name="addressbook")
 public class ContactData {
+    @Id
+    @Column(name = "id")
     private int id = Integer.MAX_VALUE;
     @Expose
+    @Column(name = "firstname")
     private  String first_name;
     @Expose
+    @Column(name = "middlename")
     private  String second_name;
     @Expose
+    @Column(name = "lastname")
     private  String last_name;
     private  String company;
     @Expose
+    @Column(name = "home")
+    @Type(type = "text")
     private  String phone;
     @Expose
+    @Transient
     private String group;
+    @Transient
     private String mobilePhone;
+    @Transient
     private String workPhone;
+    @Transient
     private String allPhones;
+    @Transient
     private String email;
+    @Transient
     private String email2;
+    @Transient
     private String email3;
+    @Transient
     private String allEmails;
-    private File photo;
+    @Column(name = "photo")
+    @Type(type = "text")
+    private String photo;
 
 
     public ContactData withId(int id) {
@@ -96,7 +117,7 @@ public class ContactData {
     }
 
     public ContactData withPhoto(File photo) {
-        this.photo = photo;
+        this.photo = photo.getPath();
         return this;
     }
 
@@ -155,7 +176,7 @@ public class ContactData {
     }
 
     public File getPhoto() {
-        return photo;
+        return new File(photo);
     }
 
     @Override
--- addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -1,5 +1,6 @@
 package kz.stqa.pft.addressbook.tests;
 
+import kz.stqa.pft.addressbook.model.ContactData;
 import kz.stqa.pft.addressbook.model.GroupData;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -39,9 +40,9 @@ public class HbConnectionTest {
     public void testHbConnections(){
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        List<GroupData> result = session.createQuery( "from GroupData" ).list();
-        for ( GroupData group : result ) {
-            System.out.println(group);
+        List<ContactData> result = session.createQuery( "from ContactData where deprecated = '0000-00-00'" ).list();
+        for ( ContactData contact : result ) {
+            System.out.println(contact);
         }
         session.getTransaction().commit();
         session.close();
