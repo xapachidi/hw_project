e2b749631da87d2f9c59843701b6ae4fd177f0de
xapachidi <apachidi.xeniya@gmail.com>
2016-07-15 16:45:32 +0600

    использование мантис

---
+++ mantis_tests/src/test/java/kz/stqa/pft/mantis/tests/TestBase.java
@@ -0,0 +1,28 @@
+package kz.stqa.pft.mantis.tests;
+
+import kz.stqa.pft.mantis.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+
+/**
+ * Created by xeniya on 6/1/16.
+ */
+public class TestBase {
+
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+
+    @BeforeSuite
+    public void setUp() throws Exception {
+        app.init();
+    }
+
+    @AfterSuite
+    public void tearDown() {
+
+        app.stop();
+    }
+
+}
