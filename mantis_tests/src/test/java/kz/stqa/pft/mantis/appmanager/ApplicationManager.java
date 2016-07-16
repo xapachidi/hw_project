22564b7ac702ebe989981744a845e6ede4c82ab1
xapachidi <apachidi.xeniya@gmail.com>
2016-07-16 17:03:46 +0600

    отправка почты

--- mantis_tests/src/test/java/kz/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis_tests/src/test/java/kz/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -24,6 +24,7 @@ public class ApplicationManager {
     private String browser;
     private RegistrationHelper registrationHelper;
     private FtpHelper ftp;
+    private MailHelper mailHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -79,5 +80,12 @@ public class ApplicationManager {
             wd.get(properties.getProperty("web.baseUrl"));
         }
         return wd;
+    }
+
+    public MailHelper mail(){
+        if (mailHelper == null){
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
     }
 }
