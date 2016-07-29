9130663f3a31d466fc8895cf8f29dcebaf8187ec
xapachidi <apachidi.xeniya@gmail.com>
2016-07-29 21:10:41 +0600

    Добавлен селениумсервер

--- addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/kz/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,14 +1,18 @@
 package kz.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
@@ -41,12 +45,19 @@ public class ApplicationManager {
 
         dbHelper = new DbHelper();
 
+        if ("".equals(properties.getProperty("selenium.server"))){
+
         if (Objects.equals(browser, BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         } else if (Objects.equals(browser, BrowserType.CHROME)){
             wd = new ChromeDriver();
         } else if (Objects.equals(browser, BrowserType.IE)){
             wd = new InternetExplorerDriver();
+        }
+        }else{
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities)
         }
 
         wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
